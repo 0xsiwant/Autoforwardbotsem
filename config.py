5cd1c01d19eb11e0676a8d076957483305e4dc59
0xsiwant <129720275+0xsiwant@users.noreply.github.com>
2024-02-05 04:31:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 # Pyrogram API credentials
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+API_ID = int(os.environ.get("18923971"))
+API_HASH = os.environ.get("78f80e014130f4ec3964550d4e87af61")
 
 # Bot token
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("6801681333:AAHmfygxg6Obl84JvDu915Nwxnu2v_2zRUk")
 
 # Pyrogram session name
-SESSION_NAME = os.environ.get("SESSION_NAME")
+SESSION_NAME = os.environ.get("Just_forwarder")
 
 # MongoDB URI (if you're using MongoDB)
-MONGODB_URI = os.environ.get("MONGODB_URI")
+MONGODB_URI = os.environ.get("mongodb+srv://kumisbot:<kumisbot>@kumis.psarpud.mongodb.net/?retryWrites=true&w=majority")
